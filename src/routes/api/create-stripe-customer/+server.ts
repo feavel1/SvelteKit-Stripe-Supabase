21a8a4503407ce0a8c289ead7ac9494bfc91c1dc
Baby Feavel <feavel@qq.com>
2023-07-24 18:00:03 +0800

    Try this

--- src/routes/api/create-stripe-customer/+server.ts
+++ src/routes/api/create-stripe-customer/+server.ts
@@ -1,27 +1,20 @@
 import { STRIPE_SECRET } from '$env/static/private';
-import { fail, json } from '@sveltejs/kit';
+import { json } from '@sveltejs/kit';
 import initStripe from 'stripe';
+import { supabase } from '$lib/supabase/subabaseClient';
 
 const stripe = new initStripe(STRIPE_SECRET as string, {
 	apiVersion: '2022-11-15'
 });
 
-export const GET = async ({ locals: { supabase, getSession } }) => {
-	// let { data: users } = await supabase.from('users').select('id');
-	const session = await getSession();
-
-	if (!session) {
-		// the user is not signed in
-		throw fail(401, { message: 'Unauthorized' });
-	}
-
+export const GET = async ({ request }) => {
 	const customer = await stripe.customers.create({
-		name: session.user.id
+		name: request.body!.record.id
 	});
 
 	const { data, error } = await supabase
 		.from('customers')
-		.insert({ id: session.user.id, stripe_customer_id: customer.id })
+		.insert({ id: request.body!.record.id, stripe_customer_id: customer.id })
 		.select();
 
 	return json({
